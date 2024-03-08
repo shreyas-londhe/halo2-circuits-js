e68bf64ce904056b52370d3c2563b727af37c68a
shryasss <shreyas_londhe@outlook.com>
2024-03-08 15:28:59 +0530

    feat: added halo2-lib

--- js/index.js
+++ js/index.js
@@ -1,10 +1,1 @@
-import("../pkg/index.js")
-    .then((module) => {
-        module.init();
-        module.initPanicHook();
-        module.initThreadPool(2);
-        const halo2wasm = new module.Halo2Wasm();
-        const myCircuit = new module.MyCircuit(halo2wasm);
-        myCircuit.run();
-    })
-    .catch(console.error);
+import("../pkg/index.js").catch(console.error)
